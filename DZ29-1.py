c498153252457c91b5ba9364020d0b30c91098dc
BanaBlack <BanaBlack228>
2024-12-28 21:21:50 +0300

    5

--- DZ29-1.py
+++ DZ29-1.py
@@ -25,16 +25,62 @@ def pars_html(html: str):
         price_watch_text = price.text.encode('latin1').decode('utf-8')
         prices.append(price_watch_text)
 
+    page_numbers_1 = soup.find('a', href="index1_page_1.html")
+    page_numbers_2 = soup.find('a', href="index1_page_2.html")
+    page_numbers_3 = soup.find('a', href="index1_page_3.html")
+    page_numbers_4 = soup.find('a', href="index1_page_4.html")
+
+    watchs = soup.find('a', href="index1_page_1.html")
+    phones = soup.find('a', href="index2_page_1.html")
+    mice = soup.find('a', href="index3_page_1.html")
+    hdd = soup.find('a', href="index4_page_1.html")
+    headphones = soup.find('a', href="index5_page_1.html")
+
+    url = 'https://parsinger.ru/html/'
+
     with open('index.txt', mode='w', encoding='utf-8') as file:
         for name, price in zip(names, prices):
             file.write(f'{name} - {price}\n')
+
+        if page_numbers_1:
+            page_numbers_1_text =(page_numbers_1['href'])
+            file.write(f'\nСсылка на первую страницу - {page_numbers_1_text}\n')
+        if page_numbers_2:
+            page_numbers_2_text =(page_numbers_2['href'])
+            file.write(f'Ссылка на первую страницу - {page_numbers_2_text}\n')
+        if page_numbers_3:
+            page_numbers_3_text =(page_numbers_3['href'])
+            file.write(f'Ссылка на первую страницу - {page_numbers_3_text}\n')
+        if page_numbers_4:
+            page_numbers_4_text =(page_numbers_4['href'])
+            file.write(f'Ссылка на первую страницу - {page_numbers_4_text}\n')
+
+        if watchs:
+            page_watchs_text =(watchs['href'])
+            watch = 'https://parsinger.ru/html/' + page_watchs_text
+            file.write(f'\nСсылка на часы - {watch}\n')
+        if phones:
+            page_phones_text =(phones['href'])
+            phone = 'https://parsinger.ru/html/' + page_phones_text
+            file.write(f'Ссылка на телефоны - {phone}\n')
+        if mice:
+            page_mice_text =(mice['href'])
+            mic = 'https://parsinger.ru/html/' + page_mice_text
+            file.write(f'Ссылка на мыши - {mic}\n')
+        if hdd:
+            page_hdd_text =(hdd['href'])
+            hddd = 'https://parsinger.ru/html/' + page_hdd_text
+            file.write(f'Ссылка на HDD - {hddd}\n')
+        if headphones:
+            page_headphones_text =(headphones['href'])
+            headphone = 'https://parsinger.ru/html/' + page_headphones_text
+            file.write(f'Ссылка на наушники - {headphone}\n')
 
 
 
 
 
     pass
-
 
 
 
